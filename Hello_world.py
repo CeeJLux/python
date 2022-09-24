3922f2d1f1f6059f3416e518838451c665710416
Emmanuel Joseph <emmjstud@gmail.com>
2022-09-24 18:53:38 +0100

    removing comments

--- Hello_world.py
+++ Hello_world.py
@@ -1,12 +1,12 @@
-# full_name = "Emmanuel Joseph"
-# age = 20
-# status = "New Patient"
-# print(full_name, age, status)
-#
-# Body = input("What is your weight: ")
-# Bag = input("What is the Weight of your Bag: ")
-# Total = float(Body) + float(Bag)
-# print("Total Weight is :" + str(Total))
+full_name = "Emmanuel Joseph"
+age = 20
+status = "New Patient"
+print(full_name, age, status)
+
+Body = input("What is your weight: ")
+Bag = input("What is the Weight of your Bag: ")
+Total = float(Body) + float(Bag)
+print("Total Weight is :" + str(Total))
 
 weight = float(input("What is your weight: "))
 unit = input("Weight in (K)gs or (L)bs: ")
