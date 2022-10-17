7bd176136ef732edbec1c4a1332dec4208b47d13
Emmanuel Joseph <emmjstud@gmail.com>
2022-10-17 20:53:30 +0100

    Game

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Starting pyton 
 Debugging codes and understanding dictionaries
+Task(s):
+Temprature app
+Bank project with OOP
+Simple game
---
+++ game.py
@@ -0,0 +1,20 @@
+# Setting up a Simple Game with Turtle
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("Black")
+
+player = turtle.Turtle()
+player.color("green")
+player.shape("square")
+player.penup()
+
+speeed = 1
+
+while True:
+    player.forward(speeed)
+
+
+
+
+
